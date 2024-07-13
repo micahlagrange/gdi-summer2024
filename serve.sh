d27f44d16bbfaee0601158c3a6461b8b6b243b55
Micah Turner <turner.micah@gmail.com>
2024-07-13 00:25:07 -0600

    name of game

--- serve.sh
+++ serve.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf makelove-build
 makelove lovejs
-unzip -o "makelove-build/lovejs/zero-days-and-nights-lovejs" -d makelove-build/html/
-echo "http://localhost:8000/makelove-build/html/zero-days-and-nights/"
+unzip -o "makelove-build/lovejs/zero-days-to-live-lovejs" -d makelove-build/html/
+echo "http://localhost:8000/makelove-build/html/zero-days-to-live/"
 python3 -m http.server
 # ruby -run -e httpd . -p 8000
